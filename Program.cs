3e62d578a97148e080f3eb8c920dc8cd7650e34f
Juan Calderon <juancalderonmicro@hotmail.com>
2025-11-20 23:56:36 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ app.MapControllers();
 
 app.Run();
 
+
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
